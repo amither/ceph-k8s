c6036d1a28bc3c73e1206526a34ded0fb97619b9
amither <arsirzhang@tencent.com>
2018-09-14 22:20:39 +0800

    v2

---
+++ v2/deploy.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+kubectl label node 172.16.0.5 node-type=storage
+kubectl label node 172.16.0.6 node-type=storage
+kubectl label node 172.16.0.13 node-type=storage
+
+kubectl create namespace ceph
+kubectl create configmap ceph-conf-combined --from-file=ceph.conf --from-file=ceph.client.admin.keyring --from-file=ceph.mon.keyring --namespace=ceph
+kubectl create configmap ceph-bootstrap-mds-keyring --from-file=ceph.keyring=ceph.mds.keyring --namespace=ceph
+kubectl create configmap ceph-bootstrap-osd-keyring --from-file=ceph.keyring=ceph.osd.keyring --namespace=ceph
+
+kubectl create -f ceph-rbac.yaml
+kubectl create -f ceph-mon-v1-svc.yaml
+kubectl create -f ceph-mon-v1-ds.yaml
+kubectl create -f daemonset-osd.yaml
+kubectl create -f deployment-mds.yaml
