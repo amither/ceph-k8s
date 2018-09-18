59b79923ac1bf1bf69f1759f1050ab627dd606c1
amither <arsirzhang@tencent.com>
2018-09-18 11:29:46 +0800

    111

--- v2/deploy.sh
+++ v2/deploy.sh
@@ -1,12 +1,13 @@
 #/bin/bash
 mv ~/.kube/config ~/.kube/config.bak
 opt="-s "https://cls-eimlt94k.ccs.tencent-cloud.com" --username=admin --password=UC9BYV3Earz5PQlC5yg9R4npOE8lHgPj --certificate-authority=cluster.ca"
-echo $opt
 kubectl $opt label node 172.16.16.11 ceph-mon=enabled ceph-osd=enabled ceph-mgr=enabled ceph-mds=enabled
 kubectl $opt label node 172.16.16.13 ceph-mon=enabled ceph-osd=enabled ceph-mgr=enabled ceph-mds=enabled
 #kubectl $opt label node 172.16.16.14 ceph-mon=enabled ceph-osd=enabled ceph-mgr=enabled ceph-mds=enabled
 
 kubectl $opt create namespace ceph
+
+#create config map
 kubectl $opt create configmap ceph-conf-combined --from-file=ceph.conf --from-file=ceph.client.admin.keyring --from-file=ceph.mon.keyring --namespace=ceph
 kubectl $opt create configmap ceph-bootstrap-mds-keyring --from-file=ceph.keyring=ceph.mds.keyring --namespace=ceph
 kubectl $opt create configmap ceph-bootstrap-osd-keyring --from-file=ceph.keyring=ceph.osd.keyring --namespace=ceph
@@ -43,6 +44,9 @@ mon_pod=`kubectl $opt -n ceph get pods | grep ceph-mon | awk '{print $1}' | head
 kubectl $opt -n ceph exec $mon_pod ceph osd pool create cephfs_data 32
 kubectl $opt -n ceph exec $mon_pod ceph osd pool create cephfs_metadata 32
 kubectl $opt -n ceph exec $mon_pod ceph fs new cephfs cephfs_metadata cephfs_data
+
+#enable dashboard
+kubectl $opt -n ceph exec $mon_pod ceph mgr module enable dashboard
  
 kubectl $opt -n ceph exec $mon_pod ceph status
 
